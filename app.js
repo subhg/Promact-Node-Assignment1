f67f745f8b9058de434a3f11a5bcc05ec53f2767
Subham Gupta <subhg2600@gmail.com>
2024-03-09 18:48:02 +0530

    added comments

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
+// Import necessary modules
 const http = require("http");
 const url = require("url");
 const { handleRoutes } = require("./route");
 
-//create server
+// Create an HTTP server
 const server = http.createServer((req, res) => {
+  // Parse the requested URL to extract the pathname
   const path = url.parse(req.url, true).pathname;
+
+  // Call the handleRoutes function to handle the incoming request based on the path
   handleRoutes(req, res, path);
 });
 
-//listen on PORT 3000
+// Define the port on which the server will listen
 const PORT = 3000;
+
+// Start the server and listen on the specified port
 server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
